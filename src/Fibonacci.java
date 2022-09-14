2168b5d145ebfb9553ac1f566fbc102db24415f2
gamal <gamaliel.g.g131@gmail.com>
2022-09-14 12:37:55 -0500

    commit 2

--- src/Fibonacci.java
+++ src/Fibonacci.java
@@ -1,5 +1,38 @@
-public class Fibonacci {
-    public Fibonacci(){
+import javax.tools.ForwardingJavaFileObject;
 
+public class Fibonacci {
+
+    public Fibonacci(){
+        setN(n);
+    }
+
+    public void setN(int n){
+        this.n=n>=0?n:0;
+    }
+
+    public int getnN(){
+        return n;
+    }
+
+    // public int fiboIterativo(){
+
+    // }
+
+
+    public int fiboRecursivo(){
+        return fibo(n);
+    }
+
+    private int fibo(int n){
+        if (n==0) {
+            // Caso Base:
+            return 0
+        } else if(n==1) {
+            // Caso Base
+            return 1;
+        }else{
+            // Caso General
+            return fibo(n-1) + fibo(n-2);
+        }
     }
 }
